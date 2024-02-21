003d745cab60bc814edbc68c83f71dc134499992
Kevin <kevinyu415@gmail.com>
2024-02-20 17:40:24 -0800

    test url

--- client/src/assets/APL_URL.js
+++ client/src/assets/APL_URL.js
@@ -1,2 +1,2 @@
-// export const API_URL = "http://localhost:8000/api/calendar"
-export const API_URL = "https://fitnessmanagement-nnfa.onrender.com"
+export const API_URL = "http://localhost:8000/api/calendar"
+// export const API_URL = "https://fitnessmanagement-nnfa.onrender.com"
